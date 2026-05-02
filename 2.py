d56e10a54ff11ae1e5673cba9046496144e7622b
IvanShumkin9876 <137620389+IvanShumkin9876@users.noreply.github.com>
2026-05-02 21:29:43 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,16 @@
+date1 = input('Введите дату: ')
+task1 = input('Введите задачу: ')
+
+date2 = input('Введите дату: ')
+task2 = input('Введите задачу: ')
+
+date3 = input('Введите дату: ')
+task3 = input('Введите задачу: ')
+
+reshenie1 = date1 + ' ' + task1
+reshenie2 = date2 + ' ' + task2
+reshenie3 = date3 + ' ' + task3
+
+print(reshenie1)
+print(reshenie2)
+print(reshenie3)

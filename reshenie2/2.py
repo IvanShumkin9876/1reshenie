ae439766b0f53af9348cd9351bfd86b8337edce4
IvanShumkin9876 <137620389+IvanShumkin9876@users.noreply.github.com>
2026-05-02 22:46:51 +0300

    Update 2.py

--- reshenie2/2.py
+++ reshenie2/2.py
@@ -1,22 +1,32 @@
+tasks = []
+today = []
+tomorrow = []
+other = []
+
 run = True
 
 while run:
 
-    tasks = []
-    today = []
-    tomorrow = []
-    other = []
+    command = input("Введите команду: ")
 
-    day = input('Введите дату: ')
-    task = input('Введите задачу: ')
-  
+    if command == "add":
 
-    if day=='Сегодня':
-        today.append(task)
-    if day=='Сегодня':
-        tomorrow.append(task)
-    if day=='Другие':
-        other.append(task)
+
+        day = input('Введите дату: ')
+        task = input('Введите задачу: ')
+
+
+        if day=='Сегодня':
+            today.append(task)
+        if day=='Завтра':
+            tomorrow.append(task)
+        if day=='Другие':
+            other.append(task)
+
+    if command == "show":
+        print(today)
+        print(tomorrow)
+        print(other)
 
     if command=='exit':
         print('Спасибо за использование! До свидания!')
